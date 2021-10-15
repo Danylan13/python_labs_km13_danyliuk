11b85a700c37e34ddb1e6639c34d9155a83ac233
Danylan13 <91112378+Danylan13@users.noreply.github.com>
2021-10-15 18:02:36 +0300

    Add files via upload

---
+++ p6_danyliuk/Python practice 6.1.py
@@ -0,0 +1,17 @@
+phrase1 = input("Input the first phrase: ").split()
+phrase2 = input("Input the second phrase: ").split()
+
+phrase1 = str(phrase1).lower()
+bywords1 = {item1 for item1 in phrase1 if item1.isalpha() }
+print ("Letters in the first phrase:", bywords1)
+
+phrase2 = str(phrase2).lower()
+bywords2 = {item2 for item2 in phrase2 if item2.isalpha() }
+print ("Letters in the second phrase:", bywords2)
+
+if bywords2 & bywords1 == bywords2:
+    print ("We can create second phrase letters from the first")
+else: 
+    print ("We can't create second phrase letters from first phrase")
+
+repeat=input('\nInput "mi scusi" to enter one more line of elements or another symbol to exit: ')
---
+++ p6_danyliuk/Python practice 6.2.py
@@ -0,0 +1,47 @@
+#hello
+
+numericbuttons = {
+    '1': ('.', ',', '?', '!', ':'),
+    '2': ('a', 'b', 'c'),
+    '3': ('d', 'e', 'f'),
+    '4': ('g', 'h', 'i'),
+    '5': ('j', 'k', 'l'),
+    '6': ('m', 'n', 'o'),
+    '7': ('p', 'q', 'r', 's'),
+    '8': ('t', 'u', 'v'),
+    '9': ('w', 'x', 'y', 'z'),
+    '0': ' '
+}
+
+def get_key(value):
+    for k, val1 in numericbuttons.items():
+        for j in val1:
+            if j == value:
+                return k, val1.index(j)
+    return None, None
+
+while True:
+    string = str(input('Input message: '))
+    print('Phone variant: ', end='')
+    for i in range(len(string)):
+        key, times = get_key(string[i].lower())
+        print(str(key)*(times+1), end='')
+
+    coc = input('\nInput "mi scusi" to enter one more line of elements or another symbol to exit: ')
+    if coc.lower() == 'mi scusi':
+        continue
+    else:
+        print('The program is finish.')
+        
+
+
+
+
+
+
+
+
+
+
+
+

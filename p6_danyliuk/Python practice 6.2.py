e94e6b3353e37d3a8d44d52b2f77c44af49450f5
Danylan13 <91112378+Danylan13@users.noreply.github.com>
2021-10-17 15:03:53 +0300

    Update Python practice 6.2.py

--- p6_danyliuk/Python practice 6.2.py
+++ p6_danyliuk/Python practice 6.2.py
@@ -21,10 +21,10 @@ def get_key(value):
     return None, None
 
 while True:
-    string = str(input('Input message: '))
+    line = str(input('Input message: '))
     print('Phone variant: ', end = '')
-    for i in range(len(string)):
-        key, times = get_key(string[i].lower())
+    for i in range(len(line)):
+        key, times = get_key(line[i].lower())
         print(str(key)*(times+1), end = '')
 
     coc = input('\nInput "mi scusi" to enter one more line of elements or another symbol to exit: ')

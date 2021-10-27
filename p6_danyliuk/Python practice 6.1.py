786a91c8615443fd9b903fbe66b88876682a7251
Danylan13 <91112378+Danylan13@users.noreply.github.com>
2021-10-27 22:53:04 +0300

    Update Python practice 6.1.py

--- p6_danyliuk/Python practice 6.1.py
+++ p6_danyliuk/Python practice 6.1.py
@@ -1,19 +1,22 @@
 while True:
-    phrase1 = input("Input the first phrase: ").split()
-    phrase2 = input("Input the second phrase: ").split()
+    phrase1 = input('Введіть перший рядок: ').split()
+    phrase2 = input('Введіть другий рядок: ').split()
+
+    bywords1 = set()
+    bywords2 = set()
 
     phrase1 = str(phrase1).lower()
-    bywords1 = {item1 for item1 in phrase1 if item1.isalpha() }
-    print ("Letters in the first phrase:", bywords1)
+    bywords1 = {words1 for words1 in phrase1 if words1.isalpha() }
+    print ('Букви в першій фразі:', bywords1)
 
     phrase2 = str(phrase2).lower()
-    bywords2 = {item2 for item2 in phrase2 if item2.isalpha() }
-    print ("Letters in the second phrase:", bywords2)
+    bywords2 = {words2 for words2 in phrase2 if words2.isalpha() }
+    print ('Букви в другій фразі :', bywords2)
 
-    if bywords2 & bywords1 == bywords2:
-        print ("We can create second phrase letters from the first")
+    if(bywords1.intersection(bywords2) == bywords2) :
+        print ('Ми можемо створити букви другої фрази з першої')
     else: 
-        print ("We can't create second phrase letters from first phrase")
+        print ('Ми не можемо створити букви другої фрази з першої')
 
     repeat=input('\nInput "mi scusi" to enter one more line of elements or another symbol to exit: ')
     if repeat == "mi scusi":

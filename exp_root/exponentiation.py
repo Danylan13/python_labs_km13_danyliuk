8c9dca55f718c74f186eb025d79c79892446af7d
Danylian13 <91112378+Danylan13@users.noreply.github.com>
2021-12-26 20:53:25 +0200

    Delete exp_root directory

--- exp_root/exponentiation.py
+++
@@ -1,8 +0,0 @@
-def exp2(n):
-    res = n ** 2
-    return res
-
-
-def exp3(n):
-    res = n ** 3
-    return res
--- exp_root/root.py
+++
@@ -1,8 +0,0 @@
-def root2(n):
-    res = n ** (1/2)
-    return res
-
-
-def root3(n):
-    res = n ** (1/3)
-    return res
